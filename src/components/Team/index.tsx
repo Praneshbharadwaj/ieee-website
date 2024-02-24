e89a98bae7bdab33087caeb2ddb8f8e83711a89e
sushma1031 <sushmajram@gmail.com>
2024-02-24 11:48:57 +0530

    Fix team image request error

--- src/components/Team/index.tsx
+++ src/components/Team/index.tsx
@@ -3,16 +3,21 @@ import SectionTitle from "../Common/SectionTitle";
 import SingleTeam from "./SingleTeam";
 
 const Team = () => {
-  const teamData: { teamName: string, paragraph: string, members: TeamType[] }[] = [
+  const teamData: {
+    teamName: string;
+    paragraph: string;
+    members: TeamType[];
+  }[] = [
     {
       teamName: "Technical Team",
-      paragraph: "Our technical team is responsible for designing and developing our products.",
+      paragraph:
+        "Our technical team is responsible for designing and developing our products.",
       members: [
         {
           id: 1,
           name: "Adveen Desuza",
           designation: "UI Designer",
-          image: "https://i.postimg.cc/rmLmxJF8/Whats-App-Image-2024-02-15-at-22-57-59-101d8153.jpg",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -30,7 +35,7 @@ const Team = () => {
           id: 11,
           name: "Jessica Lee",
           designation: "Backend Developer",
-          image: "/images/team/team-11.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -39,7 +44,7 @@ const Team = () => {
           id: 12,
           name: "Marcus Johnson",
           designation: "QA Engineer",
-          image: "/images/team/team-12.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -48,22 +53,23 @@ const Team = () => {
           id: 13,
           name: "Emily Garcia",
           designation: "Database Administrator",
-          image: "/images/team/team-13.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
         },
-      ]
+      ],
     },
     {
       teamName: "Marketing Team",
-      paragraph: "Our marketing team handles promotions and advertising strategies.",
+      paragraph:
+        "Our marketing team handles promotions and advertising strategies.",
       members: [
         {
           id: 5,
           name: "John Doe",
           designation: "Marketing Manager",
-          image: "/images/team/team-05.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -72,7 +78,7 @@ const Team = () => {
           id: 14,
           name: "Sarah Johnson",
           designation: "Marketing Coordinator",
-          image: "/images/team/team-14.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -81,7 +87,7 @@ const Team = () => {
           id: 15,
           name: "Tom Smith",
           designation: "SEO Specialist",
-          image: "/images/team/team-15.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -90,7 +96,7 @@ const Team = () => {
           id: 16,
           name: "Alice Brown",
           designation: "Content Strategist",
-          image: "/images/team/team-16.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -99,23 +105,24 @@ const Team = () => {
           id: 17,
           name: "Ryan Williams",
           designation: "Social Media Manager",
-          image: "/images/team/team-17.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
         },
-      ]
+      ],
     },
     // Add more teams with members
     {
       teamName: "Design Team",
-      paragraph: "Our design team focuses on creating visually appealing user interfaces.",
+      paragraph:
+        "Our design team focuses on creating visually appealing user interfaces.",
       members: [
         {
           id: 7,
           name: "Michael Johnson",
           designation: "Graphic Designer",
-          image: "/images/team/team-07.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -124,7 +131,7 @@ const Team = () => {
           id: 18,
           name: "Emma Watson",
           designation: "Visual Designer",
-          image: "/images/team/team-18.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -133,7 +140,7 @@ const Team = () => {
           id: 19,
           name: "Oliver Clark",
           designation: "UX Designer",
-          image: "/images/team/team-19.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -142,7 +149,7 @@ const Team = () => {
           id: 20,
           name: "Sophia Davis",
           designation: "Illustrator",
-          image: "/images/team/team-20.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -151,22 +158,23 @@ const Team = () => {
           id: 21,
           name: "Lucas Moore",
           designation: "Motion Designer",
-          image: "/images/team/team-21.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
         },
-      ]
+      ],
     },
     {
       teamName: "Content Team",
-      paragraph: "Our content team creates engaging and informative content for our users.",
+      paragraph:
+        "Our content team creates engaging and informative content for our users.",
       members: [
         {
           id: 9,
           name: "Sophia Adams",
           designation: "Content Writer",
-          image: "/images/team/team-09.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -175,7 +183,7 @@ const Team = () => {
           id: 22,
           name: "David Clark",
           designation: "Content Editor",
-          image: "/images/team/team-22.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -184,7 +192,7 @@ const Team = () => {
           id: 23,
           name: "Emma White",
           designation: "Copywriter",
-          image: "/images/team/team-23.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -193,7 +201,7 @@ const Team = () => {
           id: 24,
           name: "Jack Brown",
           designation: "Content Strategist",
-          image: "/images/team/team-24.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
@@ -202,12 +210,12 @@ const Team = () => {
           id: 25,
           name: "Lily Green",
           designation: "SEO Specialist",
-          image: "/images/team/team-25.png",
+          image: "/images/team/team-03.png",
           facebookLink: "/#",
           twitterLink: "/#",
           instagramLink: "/#",
         },
-      ]
+      ],
     },
     // Add more teams as needed
   ];
