406ac24f04eb08dc8102bc6284aab8589cc193a3
Praneshbharadwaj <praneshbharadwaj6312gmail.com>
2024-10-25 20:58:09 +0530

    deploy commit1

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['i.postimg.cc'], // Add the hostname here
-    formats: [],
+    formats: ['image/avif', 'image/webp'],
     remotePatterns: [],
   },
 };
